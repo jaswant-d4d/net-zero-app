1093438fc265a42f0900d9c3dfd7e17a7dfe7687
Jaswant Singh <jaswant.d4d@gmail.com>
2023-12-13 15:43:11 +0530

    minor changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,18 +7,18 @@ import { Outlet, useLocation } from "react-router-dom";
 const Layout = () => {
     const [bgTransparent, setBgTransparent] = useState(false);
     const location = useLocation();
-
+    const pathname = location.pathname.substring(1);
     useEffect(() => {
         const bgTransparentType = location.pathname === "/" ? true : false
         setBgTransparent(bgTransparentType)
     }, [])
 
     return (
-        <>
+        <div className={"bg-" + pathname}>
             <Header bgTransparent={bgTransparent} />
             <Outlet />
             <Footer />
-        </>
+        </div>
     )
 }
 export default Layout
