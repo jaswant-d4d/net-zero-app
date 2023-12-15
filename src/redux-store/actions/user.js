d6058bc77a5386fe7b8e77030ab19ce86df8afd8
Jaswant Singh <jaswant.d4d@gmail.com>
2023-12-15 10:21:45 +0530

    minor changes

--- src/redux-store/actions/user.js
+++ src/redux-store/actions/user.js
@@ -2,9 +2,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../api/axios";
 
 
-export const generalFormSubmit = createAsyncThunk('api/user/general/form/submit', async (data, thunkAPI) => {
+export const generalFormSubmit = createAsyncThunk('general', async (data, thunkAPI) => {
     try {
-        const response = await axios.post("/api/register", data);
+        const response = await axios.post("/api/user/general/form/submit", data);
+        return response.data;
+    } catch (error) {
+        return error;
+    }
+});   
+
+export const getCountry = createAsyncThunk('getCountry', async (data, thunkAPI) => {
+    try {
+        const response = await axios.get("/api/get/country", data);
         return response.data;
     } catch (error) {
         return error;
